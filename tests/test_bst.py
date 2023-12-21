0de18084a036199f280a3e93274a3e331ca28e93
fgshun <fgshun@gmail.com>
2023-12-21 20:16:38 +0900

    test 追加

--- tests/test_bst.py
+++ tests/test_bst.py
@@ -1,9 +1,18 @@
+import random
 import string
 
-from fgshun_bst._bst import BinarySearchTree
+import pytest
+
+from fgshun_bst._bst import BinarySearchTree as CBST
+from fgshun_bst.bst import BinarySearchTree as PyBST
 
 
-def test_bst_int():
+@pytest.fixture(params=(PyBST, CBST), ids=('Py', 'C'))
+def BinarySearchTree(request):
+    return request.param
+
+
+def test_bst_int(BinarySearchTree):
     values = [3, 1, 4, 1, 5, 9]
     py_set = frozenset(values)
 
@@ -15,7 +24,7 @@ def test_bst_int():
         assert (c in tree) is (c in py_set)
 
 
-def test_bst_str():
+def test_bst_str(BinarySearchTree):
     s = 'fgshun'
     py_set = frozenset(s)
 
@@ -27,7 +36,7 @@ def test_bst_str():
         assert (c in tree) is (c in py_set)
 
 
-def test_bst_iter():
+def test_bst_iter(BinarySearchTree):
     values = [3, 1, 4, 1, 5, 9]
 
     tree = BinarySearchTree()
@@ -37,3 +46,13 @@ def test_bst_iter():
     for a, b in zip(sorted(frozenset(values)), tree):
         assert a == b
 
+
+def test_bst_more_items(BinarySearchTree):
+    tree = BinarySearchTree()
+    r = random.Random(20231221)
+    for i in (r.randint(0, 10000000) for _ in range(10000)):
+        tree.add(i)
+    assert 0 not in tree
+    assert 7697470 in tree
+    assert 2226772 in tree
+    assert len(tree) == 9990
